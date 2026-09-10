bb250166e7a70fad3f1268f9e4792a4a1ba6bd37
Zikrulhakim <2603029@sit.singapore.edu.sg>
2026-09-10 21:08:02 +0800

    Updated

---
+++ auditor.py
@@ -0,0 +1,11 @@
+
+#variable holds inventory
+inventory = 0
+#variable holds command
+userInput = input("Enter command ")
+
+#while userInput != "quit":
+
+
+print("You have exited the inventory management system.")
+
