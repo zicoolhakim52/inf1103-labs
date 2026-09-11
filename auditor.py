a0247a172193e09d6271f392a07c51b7697294de
Zikrulhakim <2603029@sit.singapore.edu.sg>
2026-09-11 14:41:41 +0800

    Updated Lab2

--- auditor.py
+++ auditor.py
@@ -7,15 +7,18 @@ userInput = input("Please enter a valid number or 'quit' to exit: ")
 Rcount=0
 
 
-while userInput.lower() != "quit":
-    
-    if userInput.isdigit() == False:
+while userInput != "quit":
+
+    #Handles invalid input, enforce buisness rules
+    if userInput.isdigit() == False: 
         Rcount+=1
         userInput = input("\nInvalid input. Please enter a valid number or 'quit' to exit: ")     
     else:
+        #running total of inventory
         inventory += int(userInput)
         print("Current inventory: ", inventory)
-        if inventory >= 500:
+        if inventory > 500:
+            #trigger overstock alert
             print("\nALERT! Inventory limit reached.\nNo more stock can be added")
             break
         userInput = input("\nInput accepted. Please enter a valid number or 'quit' to exit: ")
