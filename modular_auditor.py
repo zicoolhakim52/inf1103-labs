d825046cdbbbbbbf43300ec4cf358deae56d8955
Zikrulhakim <2603029@sit.singaporetech.edu.sg>
2026-09-18 08:23:32 +0800

    Tax function

--- modular_auditor.py
+++ modular_auditor.py
@@ -1,4 +1,5 @@
 #Functions
+#gets user input and validates it
 def get_valid_input():
     #variable holds command
     userInput = input("Please enter a valid number or 'quit' to exit: ")
@@ -9,11 +10,14 @@ def get_valid_input():
         return("invalid")
     else:
         return(userInput)
-
+#Adds new input to inventory
 def process_delivery(current_total, new_value):
     current_total += int(new_value)
     return current_total
 
+def calculate_tax(amount):
+    amount=int(amount)*.10
+    return amount
 
 #variable holds inventory
 inventory = 0
@@ -22,19 +26,23 @@ inventory = 0
 Rcount=0
 
 while True:
-    userInput= get_valid_input()
+    if inventory < 500:
+        userInput= get_valid_input()
+    else:
+        #trigger overstock alert
+        userInput="quit"
+        print("\nALERT! Inventory limit reached.\nNo more stock can be added")
     if userInput== "quit":
+        print("Total Tax on delivery is: ", calculate_tax(inventory))
         break
     elif userInput == "invalid":
         Rcount+=1
         print("Invalid input")
     else:
         inventory=process_delivery(inventory,int(userInput))
-        print("Stock added. Current Inventory: ", inventory)
-        if inventory > 500:
-            #trigger overstock alert
-            print("\nALERT! Inventory limit reached.\nNo more stock can be added")
-            break    
+        print("Stock added. Current Inventory: ", inventory)            
+            
+                
 
 
 print("\nYou have exited the inventory management system.")
