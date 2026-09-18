4fae335bb7ad4d347ee3e563e72fb3598bce6252
Zikrulhakim <2603029@sit.singaporetech.edu.sg>
2026-09-18 15:41:42 +0800

    Modular auditor final update

--- modular_auditor.py
+++ modular_auditor.py
@@ -3,11 +3,18 @@
 def get_valid_input():
     #variable holds command
     userInput = input("Please enter a valid number or 'quit' to exit: ")
+
+    while userInput.isdigit() == False and userInput !="quit":
+        #how to count invalid input
+        print("Invalid input")
+        userInput = input("Please enter a valid number or 'quit' to exit: ")  
+        #Keep track of number of errors
+        errorCount()
     #Handles invalid input, enforce buisness rules
     if userInput=="quit":
         return("quit")
-    elif userInput.isdigit() == False:
-        return("invalid")
+    #elif userInput.isdigit() == False:
+     #   return("invalid")
     else:
         return(userInput)
 #Adds new input to inventory
@@ -17,7 +24,7 @@ def process_delivery(current_total, new_value):
 
 #calculates 10% of delivery tax
 def calculate_tax(amount):
-    amount=int(amount)*.10
+    amount=int(amount*.10)
     return amount
 
 #Prints out summary
@@ -26,15 +33,23 @@ def generate_report(total_units, failed_attempts):
     print("Total units processed: ", total_units)
     print("Total rejected entries: ", failed_attempts)
 
+#Rejected entries count
+def errorCount():
+    if not hasattr(errorCount, "count"):
+        errorCount.count=0
+    errorCount.count +=1
+    return errorCount.count
+
 #variable holds inventory
 inventory = 0
 
-#Rejected entries count
-Rcount=0
 
 while True:
     if inventory < 500:
         userInput= get_valid_input()
+        #checks if the total inventory will be more than 500
+        intentoryCheck=inventory
+        intentoryCheck+=int(userInput)
     else:
         #trigger overstock alert
         userInput="quit"
@@ -42,14 +57,15 @@ while True:
     #The only break condition to exit while loop
     if userInput== "quit":
         print("Total Tax on delivery is: ", calculate_tax(inventory))
-        generate_report(inventory,Rcount)
+        Rcount=errorCount()
+        generate_report(inventory,Rcount-1)
         break
-    elif userInput == "invalid":
-        Rcount+=1
-        print("Invalid input")
     else:
-        inventory=process_delivery(inventory,int(userInput))
-        print("Stock added. Current Inventory: ", inventory)            
+        if intentoryCheck >500:
+            print("Stock not added. Current Inventory would be more than 500: ", intentoryCheck) 
+        else:
+            inventory=process_delivery(inventory,int(userInput))
+            print("Stock added. Current Inventory: ", inventory)            
             
                 
 
